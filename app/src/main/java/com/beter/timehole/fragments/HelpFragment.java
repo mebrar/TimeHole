b10977d02ff7c0a3525d89763363cdd1862d6b58
Muhammed Ebrar Erdem <mebrarerdem@gmail.com>
2015-12-18 15:09:17 +0200

    add files

--- app/src/main/java/com/beter/timehole/fragments/HelpFragment.java
+++ app/src/main/java/com/beter/timehole/fragments/HelpFragment.java
@@ -9,8 +9,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.beter.timehole.R;
+import com.beter.timehole.core.Activity;
 
 public class HelpFragment extends Fragment {
 
@@ -20,6 +23,13 @@ public class HelpFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.help_fragment, container, false);
+
+        View helpView = inflater.inflate(R.layout.help_fragment, container, false);
+
+        String[] list = {"How to Use?", "FAQ", "End-User License Agreement", "Troubleshooting", "Send Feedback", "AboutUs"};
+        ListView helpList = (ListView) helpView.findViewById(R.id.helpList);
+        helpList.setAdapter(new ArrayAdapter(getActivity(), R.layout.support_simple_spinner_dropdown_item, list));
+
+        return helpView;
     }
 }
