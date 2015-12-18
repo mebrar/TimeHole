61d3e7a42fc8b1242be20d2c7ed4ddcb79d50511
Rumeyza Dincer <rumeyzadincer@gmail.com>
2015-12-18 23:13:18 +0200

    new colors are set

--- app/src/main/java/com/beter/timehole/fragments/ActivitiesFragment.java
+++ app/src/main/java/com/beter/timehole/fragments/ActivitiesFragment.java
@@ -42,7 +42,7 @@ public class ActivitiesFragment extends Fragment {
         tags1.add(new Tag("Calisma"));
 
         com.beter.timehole.core.Activity doneSample = new com.beter.timehole.core.Activity("ders",true,70,"16 50","17 00",tags1,"ben yaptim");
-        com.beter.timehole.core.Activity doneSample1 = new com.beter.timehole.core.Activity("ders1",false,70,"16 50","17 00",tags1,"ben yapcam1");
+        com.beter.timehole.core.Activity doneSample1 = new com.beter.timehole.core.Activity("ders1",true,70,"16 50","17 00",tags1,"ben yapcam1");
         com.beter.timehole.core.Activity doneSample2 = new com.beter.timehole.core.Activity("ders2",true,70,"16 50","17 00",tags1,"ben yaptim2");
         com.beter.timehole.core.Activity doneSample3 = new com.beter.timehole.core.Activity("ders3",false,70,"16 50","17 00",tags1,"ben yapcam3");
 
