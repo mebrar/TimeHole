382d8f33ca6c7c9a7728415dcb2b3609d030d147
Muhammed Ebrar Erdem <mebrarerdem@gmail.com>
2015-12-19 17:33:37 +0200

    added date and time picker

--- app/src/main/java/com/beter/timehole/AddReminderActivity.java
+++ app/src/main/java/com/beter/timehole/AddReminderActivity.java
@@ -1,15 +1,18 @@
 package com.beter.timehole;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.rey.material.app.DatePickerDialog;
-import com.rey.material.app.Dialog;
 
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.DatePicker;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
-import com.rey.material.app.TimePickerDialog;
 import com.rey.material.widget.EditText;
 
 
@@ -19,40 +22,59 @@ public class AddReminderActivity extends AppCompatActivity {
     EditText timePickerInput;
     static final int DATE_DIALOG_ID = 0;
     static final int TIME_DIALOG_ID = 1;
+    private Toolbar toolbar;
+    int dateYear;
+    int dateMonth;
+    int dateDay;
+    int timeHour;
+    int timeMinute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_add_reminder);
     }
-/**
-    public void
-
 
     public void datePickerClicked(View v){
         showDialog(DATE_DIALOG_ID);
+        datePickerInput = (EditText)findViewById(R.id.reminder_date_input);
+        datePickerInput.setText(dateYear + "/" + dateMonth + "/"+dateDay);
     }
 
     public void timePickerClicked(View v){
         showDialog(TIME_DIALOG_ID);
+        timePickerInput = (EditText)findViewById(R.id.reminder_time_input);
+        timePickerInput.setText(timeHour + ":" +timeMinute);
     }
 
     @Override
     protected Dialog onCreateDialog(int id){
         if(id == DATE_DIALOG_ID){
-            return new DatePickerDialog(this,)
+            return new DatePickerDialog(this,datePickerListener, dateYear,dateMonth,dateDay);
         }
         else if(id == TIME_DIALOG_ID){
-            return new TimePickerDialog(this)
+            return new TimePickerDialog(this,timePickerListener,timeHour,timeMinute,true);
         }
         return null;
     }
 
+    private DatePickerDialog.OnDateSetListener datePickerListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            dateYear = year;
+            dateMonth = monthOfYear;
+            dateDay = dayOfMonth;
+            Toast.makeText(AddReminderActivity.this,dateYear + "/" + dateMonth + "/"+dateDay,Toast.LENGTH_LONG).show();
+        }
+    };
 
-
-    private DatePickerDialog.OnDateChangedListener
-
-    */
+    private TimePickerDialog.OnTimeSetListener timePickerListener = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            timeHour = hourOfDay;
+            timeMinute = minute;
+            Toast.makeText(AddReminderActivity.this,timeHour + ":" +timeMinute,Toast.LENGTH_LONG).show();
+        }
+    };
 
 }
