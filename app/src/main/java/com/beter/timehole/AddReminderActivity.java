74e8d1bfdff0de94f01720228e44aeff64e2f6bd
Muhammed Ebrar Erdem <mebrarerdem@gmail.com>
2015-12-19 14:19:10 +0200

    addreminder change

--- app/src/main/java/com/beter/timehole/AddReminderActivity.java
+++ app/src/main/java/com/beter/timehole/AddReminderActivity.java
@@ -2,9 +2,22 @@ package com.beter.timehole;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.rey.material.app.DatePickerDialog;
 import com.rey.material.app.Dialog;
+import android.view.View;
+
+import com.rey.material.app.TimePickerDialog;
+import com.rey.material.widget.EditText;
+
 
 public class AddReminderActivity extends AppCompatActivity {
+
+    EditText datePickerInput;
+    EditText timePickerInput;
+    static final int DATE_DIALOG_ID = 0;
+    static final int TIME_DIALOG_ID = 1;
+    /**
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,4 +25,33 @@ public class AddReminderActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_add_reminder);
     }
+
+    public void
+
+
+    public void datePickerClicked(View v){
+        showDialog(DATE_DIALOG_ID);
+    }
+
+    public void timePickerClicked(View v){
+        showDialog(TIME_DIALOG_ID);
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id){
+        if(id == DATE_DIALOG_ID){
+            return new DatePickerDialog(this,)
+        }
+        else if(id == TIME_DIALOG_ID){
+            return new TimePickerDialog(this)
+        }
+        return null;
+    }
+
+
+
+    private DatePickerDialog.OnDateChangedListener
+
+    */
+
 }
