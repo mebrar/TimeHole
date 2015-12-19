aa14ba1bedba03f571cf9efa9f76745768c81d98
Muhammed Ebrar Erdem <mebrarerdem@gmail.com>
2015-12-19 18:00:08 +0200

    added filestreams

--- app/src/main/java/com/beter/timehole/AddReminderActivity.java
+++ app/src/main/java/com/beter/timehole/AddReminderActivity.java
@@ -1,6 +1,7 @@
 package com.beter.timehole;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -13,6 +14,13 @@ import android.app.TimePickerDialog;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Calendar;
+
+import com.beter.timehole.core.Reminder;
 import com.rey.material.widget.EditText;
 
 
@@ -22,29 +30,30 @@ public class AddReminderActivity extends AppCompatActivity {
     EditText timePickerInput;
     static final int DATE_DIALOG_ID = 0;
     static final int TIME_DIALOG_ID = 1;
-    private Toolbar toolbar;
-    int dateYear;
-    int dateMonth;
-    int dateDay;
-    int timeHour;
-    int timeMinute;
+    private int dateYear;
+    private int dateMonth;
+    private int dateDay;
+    private int timeHour;
+    private int timeMinute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        final Calendar cal = Calendar.getInstance();
+        dateYear = cal.get(Calendar.YEAR);
+        dateMonth = cal.get(Calendar.MONTH);
+        dateDay = cal.get(Calendar.DAY_OF_MONTH);
+
         setContentView(R.layout.activity_add_reminder);
     }
 
     public void datePickerClicked(View v){
         showDialog(DATE_DIALOG_ID);
-        datePickerInput = (EditText)findViewById(R.id.reminder_date_input);
-        datePickerInput.setText(dateYear + "/" + dateMonth + "/"+dateDay);
     }
 
     public void timePickerClicked(View v){
         showDialog(TIME_DIALOG_ID);
-        timePickerInput = (EditText)findViewById(R.id.reminder_time_input);
-        timePickerInput.setText(timeHour + ":" +timeMinute);
     }
 
     @Override
@@ -62,9 +71,10 @@ public class AddReminderActivity extends AppCompatActivity {
         @Override
         public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
             dateYear = year;
-            dateMonth = monthOfYear;
+            dateMonth = monthOfYear +1;
             dateDay = dayOfMonth;
             Toast.makeText(AddReminderActivity.this,dateYear + "/" + dateMonth + "/"+dateDay,Toast.LENGTH_LONG).show();
+            updateDatePickerText();
         }
     };
 
@@ -74,7 +84,31 @@ public class AddReminderActivity extends AppCompatActivity {
             timeHour = hourOfDay;
             timeMinute = minute;
             Toast.makeText(AddReminderActivity.this,timeHour + ":" +timeMinute,Toast.LENGTH_LONG).show();
+            updateTimePickerText();
         }
     };
+
+    private void updateDatePickerText(){
+        datePickerInput = (EditText)findViewById(R.id.reminder_date_input);
+        datePickerInput.setText(dateYear + "/" + dateMonth + "/"+dateDay);
+    }
+
+    private void updateTimePickerText(){
+        timePickerInput = (EditText)findViewById(R.id.reminder_time_input);
+        timePickerInput.setText(timeHour + ":" +timeMinute);
+    }
+
+    private void writeReminderToFile(Reminder reminder) {
+        try {
+            FileOutputStream reminderFileOutputStream = this.openFileOutput("reminderObjects", Context.MODE_PRIVATE);
+            ObjectOutputStream reminderObjectStream = new ObjectOutputStream(reminderFileOutputStream);
+            reminderObjectStream.writeObject(reminder);
+            reminderObjectStream.close();
+            reminderFileOutputStream.close();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 
 }
