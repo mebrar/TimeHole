c51154a5805ee5432b4b0ae3513727a45e743718
Taha <tahaaksu01@gmail.com>
2015-12-19 14:39:21 +0200

    Toolbar(addReminder+addActivity)

--- app/src/main/java/com/beter/timehole/AddReminderActivity.java
+++ app/src/main/java/com/beter/timehole/AddReminderActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 import com.rey.material.app.DatePickerDialog;
 import com.rey.material.app.Dialog;
+
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.rey.material.app.TimePickerDialog;
@@ -17,15 +19,17 @@ public class AddReminderActivity extends AppCompatActivity {
     EditText timePickerInput;
     static final int DATE_DIALOG_ID = 0;
     static final int TIME_DIALOG_ID = 1;
-    /**
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
 
         setContentView(R.layout.activity_add_reminder);
     }
-
+/**
     public void
 
 
