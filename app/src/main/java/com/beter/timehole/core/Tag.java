dbdd8966539e821df53d2d2692d193032804b9e1
Muhammed Ebrar Erdem <mebrarerdem@gmail.com>
2015-12-19 11:43:46 +0200

    added tag serialuid

--- app/src/main/java/com/beter/timehole/core/Tag.java
+++ app/src/main/java/com/beter/timehole/core/Tag.java
@@ -6,6 +6,7 @@ package com.beter.timehole.core;
 public class Tag implements java.io.Serializable {
 
     private String tagName;
+    public static final long serialVersionUID = 12341234L;
 
     public Tag(String tagName){
         this.tagName=tagName;
