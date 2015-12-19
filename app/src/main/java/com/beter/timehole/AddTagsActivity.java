2084c4c80f4d61592762c238644324d4fdf45bad
Rumeyza <rumeyzadincer@gmail.com>
2015-12-19 23:05:50 +0200

    add drawable things

---
+++ app/src/main/java/com/beter/timehole/AddTagsActivity.java
@@ -0,0 +1,33 @@
+package com.beter.timehole;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.view.View.OnClickListener;
+import android.view.View;
+import com.beter.timehole.fragments.SetTagDialogFragment;
+
+
+public class AddTagsActivity extends AppCompatActivity {
+
+    private Button dfragbutton;
+    private FragmentManager fm = getSupportFragmentManager();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_tags);
+
+        dfragbutton = (Button) findViewById(R.id.dfragbutton);
+
+        dfragbutton.setOnClickListener(new OnClickListener() {
+            public void onClick(View arg0) {
+                SetTagDialogFragment dFragment = new SetTagDialogFragment();
+               // Show DialogFragment
+               // SetTagDialogFragment.show(fm,"Dialog Fragment");
+            }
+        });
+    }
+}
